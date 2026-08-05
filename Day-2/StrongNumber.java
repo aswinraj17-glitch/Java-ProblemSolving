efd5082dddd93aa5c1ae57b5bcc19b4935cb3d02
Aswin Raj D <aswinraj790417@gmail.com>
2026-08-05 23:43:08 +0530

    Add files via upload

---
+++ Day-2/StrongNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class StrongNumber{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int start=sc.nextInt();
+        int end=sc.nextInt();
+
+        for (int num=start;num<=end;num++) {
+            int temp=num;
+            int sum=0;
+
+            while(temp>0) {
+                int digit=temp%10;
+                int fact=1;
+                for(int i=1;i<=digit;i++) {
+                    fact*=i;
+                }
+
+                sum+=fact;
+                temp/=10;
+            }
+
+            if(num==1 ||sum==num) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
