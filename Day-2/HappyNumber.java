b50c6b8965032ca54b46d464919d9e6dc1c2fd75
Aswin Raj D <aswinraj790417@gmail.com>
2026-08-06 22:09:27 +0530

    Add files via upload

---
+++ Day-2/HappyNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class HappyNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        for (int i = start; i <= end; i++) {
+            int temp = i;
+            while (temp != 1 && temp != 4) {
+                int sum = 0;
+                while (temp > 0) {
+                    int digit = temp % 10;
+                    sum += digit * digit;
+                    temp = temp / 10;
+                }
+                temp = sum;
+            }
+            if (temp == 1) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
