b413503352088a2ec6cc92c28d0dea946798a0bf
Aswin Raj D <aswinraj790417@gmail.com>
2026-08-08 21:01:47 +0530

    Update HappyNumber.java

--- Day-2/HappyNumber.java
+++ Day-2/HappyNumber.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class HappyNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        
         int start = sc.nextInt();
         int end = sc.nextInt();
         for (int i = start; i <= end; i++) {
