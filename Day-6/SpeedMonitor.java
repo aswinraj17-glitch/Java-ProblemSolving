e0db3ab5f21b69f9b50e3fec27a8ab7db8e50317
Aswin Raj D <aswinraj790417@gmail.com>
2026-08-25 09:53:29 +0530

    Add files via upload

---
+++ Day-6/SpeedMonitor.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class SpeedMonitor{
+    public static  void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int S=sc.nextInt();
+        int L=sc.nextInt();
+        int R=sc.nextInt();
+        
+        int effectiveLimit=L;
+        if(R==1){
+            effectiveLimit=L-10;
+        }
+        
+        int excess= S - effectiveLimit;
+        if(excess<=0){
+            System.out.println("SAFE");
+        }
+        else if(excess<=10){
+            System.out.println("WARNING");
+            
+        }
+        else if(excess<=30){
+            System.out.println("FINE 1000");
+            
+        }else{
+            System.out.println("FINE 3000");
+            
+        }
+        
+        
+    }
+}
