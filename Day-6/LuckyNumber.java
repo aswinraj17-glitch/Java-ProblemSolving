8aee13b2f7d3f11abc85c8c6c37d9ae659264dbc
Aswin Raj D <aswinraj790417@gmail.com>
2026-08-27 20:24:19 +0530

    Add files via upload

---
+++ Day-6/LuckyNumber.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class LuckyNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        boolean lucky = true;
+
+        while (n > 0) {
+
+            long digit = n % 10;
+            long temp = n / 10;
+
+            while (temp > 0) {
+                long nextDigit = temp % 10;
+
+                if (digit == nextDigit) {
+                    lucky = false;
+                    break;
+                }
+
+                temp = temp / 10;
+            }
+
+            if (!lucky) {
+                break;
+            }
+
+            n = n / 10;
+        }
+
+        if (lucky) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
