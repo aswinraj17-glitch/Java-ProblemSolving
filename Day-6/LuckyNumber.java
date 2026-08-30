3518a0d4f1533975d3b6b18b37ed33eaee406fc3
Aswin Raj D <aswinraj790417@gmail.com>
2026-08-30 22:49:57 +0530

    Refactor LuckyNumber.java for consistency

--- Day-6/LuckyNumber.java
+++ Day-6/LuckyNumber.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class LuckyNumber {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         long n = sc.nextLong();
