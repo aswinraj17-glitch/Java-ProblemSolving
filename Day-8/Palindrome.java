ca69925f8279644cd0ea50c2d646a2e6b5c81dce
Aswin Raj D <aswinraj790417@gmail.com>
2026-09-06 18:00:00 +0530

    Add Java problems for September 6

---
+++ Day-8/Palindrome.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Palindrome {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+
+        String temp=s;
+        String rev="";
+        for(int i=s.length()-1;i>=0;i--){
+            rev+=s.charAt(i);
+        
+        }
+        // System.out.println(temp);
+        if(temp.equals(rev)){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not a Palindrome");
+
+        }
+        sc.close();
+    }
+}
---
+++ Day-8/ReverseString.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ReverseString{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        // char[]arr=s.toCharArray();
+        for(int i=s.length()-1;i>=0;i--){
+             System.out.print(s.charAt(i));
+        }
+        sc.close();
+        
+            }
+}
